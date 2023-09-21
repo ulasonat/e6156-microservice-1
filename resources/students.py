192374b9bc7b89904a82caedd6d92479545ba0cd
ulasonat <ulasonat11@gmail.com>
2023-09-20 20:53:13 -0400

    Change dir address

--- resources/students.py
+++ resources/students.py
@@ -8,7 +8,7 @@ class StudentsResource:
     #
 
     students_file = \
-        "/Users/donaldferguson/Dropbox/000-Examples/e6156-microservice-1/resources/old-students.json"
+        "/Users/ulasonat/Desktop/cloud_computing/e6156-microservice-1/resources/old-students.json"
 
     def __init__(self):
         self.students = None
